5a951bba2eacbd8017a8836dfb93893f18e9f793
Ehsan Houshmand <houshmand2007@gmail.com>
2019-02-09 15:31:13 +0330

    Initial wordpress plugin file

---
+++ pod-sso-plugin.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Plugin Name: Pod SSO Plugin
+ * Plugin URI:  http://docs.pod.land
+ * Description: Login with pod account
+ * Version:     1.0.0
+ * Author:      Ehsan Houshmand
+ * Author URI:  http://ehsanhoushmand.ir
+ * License:     GPL2
+ */
